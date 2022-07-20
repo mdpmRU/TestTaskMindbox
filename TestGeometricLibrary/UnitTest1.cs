2b3184f6debfa5f8993145dc5f314f71f0904ca1
mdpmRU <udanuda@gmail.com>
2022-07-20 13:02:30 +0300

    Добавление unit тестов

---
+++ TestGeometricLibrary/UnitTest1.cs
@@ -0,0 +1,39 @@
+using System;
+using Xunit;
+using GeometricLibrary.Entities;
+using GeometricLibrary.Services;
+
+namespace TestGeometricLibrary
+{
+    public class UnitTest1
+    {
+        TriangleService triangleService = new TriangleService();
+        CircleService circleService = new CircleService();
+        [Fact]
+        public void TestSquareTirangle()
+        {
+            double[] sides = { 7, 24, 25 };
+            var triangle = new Triangle(sides);
+            double triangleSquareExpectation = 84;
+            Assert.Equal(triangleSquareExpectation, triangleService.Square(triangle));
+        }
+
+        [Fact]
+        public void TestSquareCicrcle()
+        {
+            double radius = 3;
+            var circle = new Circle(radius);
+            double circleSquareExpectation = 28.2743339;
+            Assert.Equal(circleSquareExpectation, Math.Round(circleService.Square(circle),7));
+        }
+
+        [Fact]
+        public void TestSquareness—heck()
+        {
+            double[] sides = { 7, 24, 25 };
+            var triangle = new Triangle(sides);
+            bool triangleSquarenessExpectation = true;
+            Assert.Equal(triangleSquarenessExpectation, triangleService.Squareness—heck(triangle));
+        }
+    }
+}
